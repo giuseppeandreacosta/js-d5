c85d1dd2074da30db3cf4e7ce8902a8b475a0585
giuseppeandreacosta <giuseppeandreac@gmail.com>
2023-07-04 21:22:17 +0200

    modifiche

--- exercise.js
+++ exercise.js
@@ -37,10 +37,10 @@ console.log(boundary(200));
 // /* SCRIVI QUI LA TUA RISPOSTA */
 // // ciclo for con i--( decrescente )
 // // .lenght -1
-function reverseString (str) {
+function reverseString(str) {
   let newString = "";
   for (let i = str.length - 1; i >= 0; i--) {
-      newString += str[i];
+    newString += str[i];
   }
   return newString;
 }
@@ -52,26 +52,21 @@ console.log(reverseString("Kawasaki"));
 function reverseString(str) {
   return str.split("").reverse().join("");
 }
-console.log(reverseString("KAWASAKI"));
+console.log(reverseString("cipolla"));
 
 // /* ESERCIZIO 4
 //  Scrivi una funzione chiamata "upperFirst", che accetta una stringa come parametro e la ritorna rendendo maiuscola ogni lettera iniziale di ogni parola.
 // */
 
-
 // /* SCRIVI QUI LA TUA RISPOSTA */
-function upperFirst(){
-    let string = "maiuscolo";
-    return upperFirst.toUpperCase()  ;
+
+function upperFirst(up){
+  let maiuscolo = "";
+  for (let i = 0; i < maiuscolo.length; i++){
   }
-  console.log(upperFirst);
-
-//   function upperFirst(maiuscolo){
-
-//     return upperFirst.toUpperCase(maiuscolo)  ;
-//   }
-//   let string = upperFirst;
-//   console.log(upperFirst);
+  return up.toUpperCase();
+}
+console.log(upperFirst("maiuscolo,minuscolo"));
 
 // /* ESERCIZIO 5
 //  Scrivi una funzione chiamata "giveMeRandom", che accetta come parametro un numero chiamato n e ritorna un array contenente n numeri random contenuti tra 0 e 10.
@@ -79,15 +74,15 @@ function upperFirst(){
 
 // /* SCRIVI QUI LA TUA RISPOSTA */
 
-// function giveMeRandom(n) {
-//     let random = n;
-//     for (let i = 0;i < 10;i++){
-//         random += giveMeRandom[i];
-//     }
-//     return random;
-// }
-// giveMeRandom(n)
-// console.log(giveMeRandom);
+function giveMeRandom(n) {
+  let random = [];
+  for (let i = 0; i < n; i++) {
+    random.push(Math.floor(Math.random() * 11));
+  }
+  return random;
+}
+
+console.log(giveMeRandom(3));
 
 // //EXTRA:
 // /* ESERCIZIO 1
@@ -96,6 +91,11 @@ function upperFirst(){
 
 // /* SCRIVI QUI LA TUA RISPOSTA */
 
+function area(l1, l2) {
+  return l1 * l2;
+}
+console.log(area);
+
 // /* ESERCIZIO 2
 //  Scrivi una funzione chiamata "crazyDiff" che calcola la differenza assoluta tra un numero fornito e 19.
 //  Se il valore calcolato è più grande di 19, la funzione deve tornare tale risultato moltiplicato per 3.
@@ -103,12 +103,31 @@ function upperFirst(){
 
 // /* SCRIVI QUI LA TUA RISPOSTA */
 
+function crazyDiff(x,y){
+  if (y > x) {
+    return (x + y) * 3;
+  } else {
+    return x + y;
+  }
+}
+const sumOne = crazyDiff(-31,19)
+console.log(sumOne);
+
+
+
 // /* ESERCIZIO 3
 //  Scrivi una funzione chiamata "codify" che accetta una stringa come parametro.
 //  La funzione deve aggiungere la parola "code" all'inizio della stringa fornita e ritornare il risultato, ma se la stringa fornita comincia proprio con "code" allora deve ritornarla senza modifiche.
 // */
 
 // /* SCRIVI QUI LA TUA RISPOSTA */
+function codify(string){
+  if (string.startsWith("code")){
+    return string;
+  } else {
+    return "code" + string
+  } 
+}
 
 // /* ESERCIZIO 4
 //  Scrivi una funzione chiamata "check3and7" la quale accetta un numero intero positivo come parametro.
@@ -118,8 +137,23 @@ function upperFirst(){
 
 // /* SCRIVI QUI LA TUA RISPOSTA */
 
+function check3and7(){
+  if (number % 3 === 0 || number % 7 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
 // /* ESERCIZIO 5
 //  Scrivi una funzione chiamata "cutString", che accetta una stringa come parametro e la ritorna senza il primo e l'ultimo carattere.
 // */
 
 // /* SCRIVI QUI LA TUA RISPOSTA */
+
+// function cutString(stringCut) {
+//   if 
+
+// }
+// let stringCut = "stringa"
